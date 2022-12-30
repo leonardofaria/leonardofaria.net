0ad6b43e72ee585ee8843ced27c84cc1e2c2aab0
Leonardo Faria <leonardofaria@gmail.com>
2022-12-30 11:18:29 -0800

    Fix footer message

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,21 +1,13 @@
 export default function Footer() {
   return (
     <footer className="w-full max-w-7xl mx-auto text-center border-t border-gray-200 py-9 px-3 mt-9 pin-b text-sm text-gray-500">
-      Made with{' '}
+      Made with Next.js in the beautiful Vancouver, CA.{' '}
       <a
         className="underline transition duration-300 ease-in-out hover:text-blue-600"
-        href="https://gohugo.io"
+        href="https://github.com/leonardofaria/leonardofaria.net"
       >
-        Hugo
-      </a>{' '}
-      and{' '}
-      <a
-        className="underline transition duration-300 ease-in-out hover:text-blue-600"
-        href="https://github.com/leonardofaria/bento"
-      >
-        Bento theme
+        Source-code
       </a>
-      .
     </footer>
   );
 }
