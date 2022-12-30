d76ecbb64f3352886e6b85cee37e1340faf5f6c5
Leonardo Faria <leonardofaria@gmail.com>
2022-12-30 15:52:37 -0800

    Add Webmention info

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { Inter } from '@next/font/google';
 import SEO from 'lib/next-seo.config';
 import Script from 'next/script';
 import { UMAMI_SITEID, UMAMI_URL } from 'lib/constants';
+import Head from 'next/head';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -16,7 +17,15 @@ function MyApp({ Component, pageProps }: AppProps) {
     <div
       className={`${inter.variable} font-sans bg-gradient flex flex-col min-h-screen`}
     >
+      <Head>
+        <link href="https://webmention.io/leonardofaria/webmention" />
+        <link
+          href="https://webmention.io/leonardofaria/xmlrpc"
+          rel="pingback"
+        />
+      </Head>
       <DefaultSeo {...SEO} />
+
       <Component {...pageProps} />
 
       <Script
