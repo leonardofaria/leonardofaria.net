77fab4dabf5e4b268160617f6923794dc83c2c7b
Leonardo Faria <leonardofaria@gmail.com>
2023-01-08 12:35:40 -0800

    Fix screenshot generation

--- pages/api/thumbnail.ts
+++ pages/api/thumbnail.ts
@@ -5,22 +5,27 @@ import { getAbsoluteURL } from 'lib/utils';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const browser = await playwright.launchChromium();
-  const page = await browser.newPage({
-    viewport: {
-      width: 1200,
-      height: 630,
-    },
-  });
-  const url = getAbsoluteURL((req.query.path as string) || '');
-  await page.goto(url, {
-    timeout: 0,
-  });
-  const data = await page.screenshot({
-    type: 'png',
-  });
-  setTimeout(() => browser.close(), 0);
-  res.setHeader('cache-control', 's-maxage=60, stale-while-revalidate');
-  res.setHeader('Content-Type', 'image/png');
-  res.end(data);
+  try {
+    const browser = await playwright.launchChromium({ headless: true });
+    const page = await browser.newPage({
+      viewport: {
+        width: 1200,
+        height: 630,
+      },
+    });
+    const url = getAbsoluteURL((req.query.path as string) || '');
+    await page.goto(url, {
+      timeout: 0,
+    });
+    const data = await page.screenshot({
+      type: 'png',
+    });
+    setTimeout(() => browser.close(), 0);
+    res.setHeader('cache-control', 's-maxage=60, stale-while-revalidate');
+    res.setHeader('Content-Type', 'image/png');
+    res.end(data);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log({ error });
+  }
 };
