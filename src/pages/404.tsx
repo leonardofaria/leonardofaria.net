b7a94c616bb5a1ec567418a353422f0ff6aea2cf
Leonardo Faria <leonardofaria@gmail.com>
2023-02-02 22:03:17 -0800

    Add 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,17 +5,23 @@ export default function NotFound() {
     <>
       <Header />
 
-      <main className="mx-auto mt-32 w-full max-w-3xl flex-1 text-gray-700">
-        <article className="article">
-          <h1 className="leading-9">Not found</h1>
-
-          <div className="article__content">
-            <div className="mb-16">
-              <p>Error 404: Page not found</p>
+      <div className="mx-auto max-w-max">
+        <main className="sm:flex">
+          <p className="text-4xl font-bold tracking-tight text-indigo-600 sm:text-5xl">
+            404
+          </p>
+          <div className="sm:ml-6">
+            <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+                Page not found
+              </h1>
+              <p className="mt-1 text-base text-gray-500">
+                Please check the URL in the address bar and try again.
+              </p>
             </div>
           </div>
-        </article>
-      </main>
+        </main>
+      </div>
 
       <Footer />
     </>
