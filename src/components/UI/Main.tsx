30d99f4db35009ecba6b097377cc9335539d32b3
Leonardo Faria <leonardofaria@gmail.com>
2023-02-02 22:31:35 -0800

    Fix 404

--- src/components/UI/Main.tsx
+++ src/components/UI/Main.tsx
@@ -1,9 +1,16 @@
 import { ReactNode } from 'react';
 
-export function Main({ children, ...rest }: { children: ReactNode }) {
+export function Main({
+  children,
+  className,
+  ...rest
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
   return (
     <main
-      className="mx-auto mt-24 w-full max-w-7xl flex-1 text-gray-700"
+      className={`mx-auto mt-24 w-full max-w-7xl flex-1 text-gray-700 ${className}`}
       {...rest}
     >
       {children}
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,27 +1,27 @@
-import { Footer, Header } from 'src/components/UI';
+import { Footer, Header, Main } from 'src/components/UI';
 
 export default function NotFound() {
   return (
     <>
       <Header />
 
-      <div className="mx-auto max-w-max">
-        <main className="sm:flex">
-          <p className="text-4xl font-bold tracking-tight text-indigo-600 sm:text-5xl">
+      <Main className="flex items-center justify-center">
+        <div className="mx-auto flex max-w-max">
+          <p className="text-4xl font-semibold tracking-tighter text-gray-900 sm:text-5xl">
             404
           </p>
           <div className="sm:ml-6">
             <div className="sm:border-l sm:border-gray-200 sm:pl-6">
-              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+              <h1 className="text-4xl font-semibold tracking-tighter text-gray-700 sm:text-5xl">
                 Page not found
               </h1>
-              <p className="mt-1 text-base text-gray-500">
+              <p className="mt-3 text-base text-gray-500">
                 Please check the URL in the address bar and try again.
               </p>
             </div>
           </div>
-        </main>
-      </div>
+        </div>
+      </Main>
 
       <Footer />
     </>
