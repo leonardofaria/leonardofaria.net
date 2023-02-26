12a1cd93386efc46962c30c3791fa3eb36b2403c
Leonardo Faria <leonardofaria@gmail.com>
2023-02-26 00:02:47 -0800

    Add last update info

--- src/components/UI/Footer.tsx
+++ src/components/UI/Footer.tsx
@@ -1,17 +1,28 @@
 import { SocialNav } from './SocialNav';
+import commits from '../../../public/commits.json';
 
 export function Footer() {
+  const lastCommitDate = new Date(commits[0]?.commit?.committer?.date);
+
   return (
     <footer className="mx-auto w-full max-w-7xl lg:px-4">
       <div className="flex flex-col items-center justify-between gap-3 border-t border-charade-300 p-4 text-sm text-charade-500 lg:flex-row">
-        <p>
-          <a
-            className="underline transition duration-300 ease-in-out hover:text-blue-600"
-            href="https://github.com/leonardofaria/leonardofaria.net"
-          >
-            Made
-          </a>{' '}
-          in the beautiful Vancouver, CA
+        <p className="flex flex-col lg:flex-row">
+          <span className="text-center">
+            <a
+              className="underline transition duration-300 ease-in-out hover:text-blue-600"
+              href="https://github.com/leonardofaria/leonardofaria.net"
+            >
+              Made
+            </a>{' '}
+            in the beautiful Vancouver, CA.&nbsp;
+          </span>
+          <span className="text-center">
+            Last update:{' '}
+            {new Intl.DateTimeFormat('en', {
+              dateStyle: 'medium',
+            }).format(lastCommitDate)}
+          </span>
         </p>
         <nav>
           <SocialNav />
---
+++ src/lib/githubCommits.ts
@@ -0,0 +1,16 @@
+import { writeFileSync } from 'fs';
+
+(async () => {
+  const request = await fetch(
+    'https://api.github.com/repos/leonardofaria/leonardofaria.net/commits'
+  );
+
+  const commits = await request.json();
+
+  writeFileSync('./public/commits.json', JSON.stringify(commits, null, 2));
+
+  // eslint-disable-next-line no-console
+  console.log('Response saved to file');
+})();
+
+export {}; // Empty export because I have to
