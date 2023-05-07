f00ac6005105e27c34a9b526b518915048073659
Leonardo Faria <leonardofaria@gmail.com>
2023-05-06 22:39:55 -0700

    Add animated header

--- src/components/UI/PageNav.tsx
+++ src/components/UI/PageNav.tsx
@@ -1,36 +1,55 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+
+const pages = [
+  {
+    name: 'Microblog',
+    value: '/microblog',
+  },
+  {
+    name: 'Archives',
+    value: '/archives',
+  },
+  {
+    name: 'Talks',
+    value: '/talks',
+  },
+];
 
 export function PageNav() {
   const router = useRouter();
-
-  const pages = [
-    {
-      name: 'Microblog',
-      value: '/microblog',
-    },
-    {
-      name: 'Archives',
-      value: '/archives',
-    },
-    {
-      name: 'Talks',
-      value: '/talks',
-    },
-  ];
+  const initialTab = pages.find((page) => router.asPath.includes(page.value));
+  const [activeTab, setActiveTab] = useState<string | null>(
+    initialTab?.name ?? null
+  );
 
   return (
     <ul className="flex w-full flex-col items-end justify-end lg:flex-row lg:items-center">
       {pages.map((page) => (
         <li
-          className={`rounded-full border border-transparent transition duration-300 ease-in-out hover:bg-white ${
+          className={`relative rounded-full border border-transparent transition duration-300 ease-in-out ${
             router.asPath.includes(page.value)
               ? 'bg-amethyst-smoke-200 shadow-inner'
               : null
           }`}
           key={page.name}
+          onMouseLeave={() => setActiveTab(initialTab?.name ?? null)}
         >
-          <Link className="block p-2 px-4" href={page.value}>
+          {activeTab === page.name && (
+            <motion.span
+              className="absolute inset-0 -z-10 bg-white"
+              layoutId="bubble"
+              style={{ borderRadius: 9999 }}
+              transition={{ type: 'spring', duration: 0.6 }}
+            />
+          )}
+          <Link
+            className="relative block p-2 px-4"
+            href={page.value}
+            onMouseOver={() => setActiveTab(page.name)}
+          >
             {page.name}
           </Link>
         </li>
